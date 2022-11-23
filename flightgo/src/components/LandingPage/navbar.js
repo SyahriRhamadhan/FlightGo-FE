429cc15f882191e5e189e4b17e8e2245a804bc0c
imy18 <imyproject18@gmail.com>
2022-11-24 06:13:29 +0700

    first commit

---
+++ flightgo/src/components/LandingPage/navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      Ini menu LandingPage dikerjakan hilmy, semangat!
+    </div>
+  )
+}
+
+export default Navbar
---
+++ flightgo/src/components/Login/login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+      Ini menu login dikerjakan fatia, semangat.
+    </div>
+  )
+}
+
+export default Login
